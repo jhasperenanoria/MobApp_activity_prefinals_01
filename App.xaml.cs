13514fc3e1e4d440a6d538bb91b1b73b55068049
jhasperenanoria <82790991+jhasperenanoria@users.noreply.github.com>
2023-11-15 17:38:10 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace labex_04_mobApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace labex_04_mobApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
